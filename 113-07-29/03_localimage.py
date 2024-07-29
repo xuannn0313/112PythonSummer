840085c9798b1d0843f6cbb8e957efac2b1e77e9
xuannn0313 <sharon5200103@gmail.com>
2024-07-29 16:01:43 +0800

    113-07-29

--- 113-07-29/03_localimage.py
+++ 113-07-29/03_localimage.py
@@ -4,6 +4,6 @@ URL='https://notify-api.line.me/api/notify'
 H,P,F={},{},{}
 H['Authorization']='Bearer 3dMhDEbEzaee9HSeZ8F7Br11IxI7FomZiG7BIJCS6f4'
 P['message']='本機圖片'
-F['imageFile']=open('Chiikawa-62820447ac7d4.jpg','rb')
-response=requests.post(URL,headers=H,params=P)
+F['imageFile']=open('Chiikawa.jpg','rb')
+response=requests.post(URL,headers=H,params=P,files=F)
 
